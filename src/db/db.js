0b5de59fe0389ee8df4ef9ef84a5693b990ddff4
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-06-14 22:24:43 +0530

    commited

--- src/db/db.js
+++ src/db/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 async function connectDB() {
   await mongoose.connect("mongodb+srv://as:HuhNZNpBvqJEbOuM@cluster0.v2mwqfo.mongodb.net/halley");
 
-  console.log("connect to db");
+  console.log("connect to MongoDB");
 }
 
 module.exports = connectDB;
